9c1c3a6bd31fb8565a726da38fbba0c4b3769528
qmaster0803 <qmaster080305@gmail.com>
2020-09-30 23:18:43 +0300

    Added running exp filter in search mode

--- radiation_monitor.ino
+++ radiation_monitor.ino
@@ -4,14 +4,13 @@
 #define FILTER_COEF 0.2
 
 unsigned long particle_counter = 0;
-unsigned long particle_displayed_1 = -1;
-unsigned long particle_displayed_2 = -1;
+unsigned long particle_displayed = -1;
 unsigned long particle_start_3 = 0;
 unsigned long last_particle_time = 0;
 int last_mode = 0;
 bool lock_3_mode = false;
 
-unsigned long part_times[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+unsigned long part_time_avg = 0;
 
 float expRunningAverage(float newVal) {
   static float filVal = 0;
@@ -33,34 +32,17 @@ void loop()
   switch(mode)
   {
     case 1:
-      if((particle_counter != particle_displayed_1) || last_mode != 1)
+      if(particle_counter != particle_displayed || last_mode != 1)
       {
         displayInt(2, particle_counter);
-        particle_displayed_1 = particle_counter;
-        for(int i = 0; i < 10; i++)
-        {
-          Serial.print(part_times[i]);
-          Serial.print(" ");
-        }
-        Serial.println("");
+        particle_displayed = particle_counter;
       }
     break;
     case 2:
-      if(particle_counter < 11 && last_mode != 2)
+      if(particle_counter != particle_displayed || last_mode != 2)
       {
-        displayDigits(2, QD_MINUS, QD_MINUS, QD_MINUS, QD_MINUS); 
-      }
-      else if(particle_counter >= 11 && particle_displayed_2 != particle_counter)
-      {
-        displayFloat(2, double(60000)/(double(part_times[0]+part_times[1]+part_times[2]+part_times[3]+part_times[4])/double(6))/double(1.08), 1);
-        float now_val = double(60000)/(double(part_times[0]+part_times[1]+part_times[2]+part_times[3]+part_times[4])/double(6))/double(1.08);
-        
-        particle_displayed_2 = particle_counter;
-      }
-      if(particle_counter > 10 && last_mode != 2)
-      {
-        displayFloat(2, double(60000)/(double(part_times[0]+part_times[1]+part_times[2]+part_times[3]+part_times[4])/double(6))/double(1.08), 1);
-        particle_displayed_2 = particle_counter;
+        displayFloat(2, (float(60000)/float(part_time_avg))/float(CONVERSION_COEF), 1);
+        particle_displayed = particle_counter;
       }
     break;
     case 3:
@@ -91,30 +73,9 @@ void loop()
   if(analogRead(A0) > 200)
   {
     particle_counter++;
-    float now_val = millis() - last_particle_time;
-    Serial.print(now_val);
-    Serial.print(" ");
-    Serial.println(expRunningAverage(now_val));
     delayMicroseconds(5000); 
-    if(particle_counter < 11)
-    {
-      part_times[particle_counter-1] = millis() - last_particle_time;
-      last_particle_time = millis();
-    }
-    else
-    {
-      part_times[0] = part_times[1];
-      part_times[1] = part_times[2];
-      part_times[2] = part_times[3];
-      part_times[3] = part_times[4];
-      part_times[4] = part_times[5];
-      part_times[5] = part_times[6];
-      part_times[6] = part_times[7];
-      part_times[7] = part_times[8];
-      part_times[8] = part_times[9];
-      part_times[9] = millis() - last_particle_time;
-      last_particle_time = millis();
-    }
+    part_time_avg = expRunningAverage(millis() - last_particle_time);
+    last_particle_time = millis();
   }
 }
 
